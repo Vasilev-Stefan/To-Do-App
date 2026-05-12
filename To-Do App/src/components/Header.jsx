c0737ffdc787ee9623efc53471aee076eae63161
Vasilev-Stefan <sbasilev@gmail.com>
2026-05-12 20:48:44 +0300

    Add Header component

---
+++ To-Do App/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="flex items-center justify-center bg-gray-400 h-10">
+            <p className="text-xl font-bold">To-Do App</p>
+        </div>
+    )
+}
